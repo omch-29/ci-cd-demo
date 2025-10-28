777b023f3a07add7a911997ef643c96a4d093f8e
omch-29 <chandwaniom29@gmail.com>
2025-10-28 15:56:46 +0530

    changed sum function

--- sum.test.js
+++ sum.test.js
@@ -5,7 +5,10 @@ describe("test for sum function",()=>{
     test("add 3+2 equals 5",()=>{
     expect(sum(3,2)).toBe(5);
 });
-test("add -1+2 equals 5",()=>{
+test("add -1+2 equals 1",()=>{
     expect(sum(-1,2)).toBe(1);
+});
+test("add -5+-5 equals -10",()=>{
+    expect(sum(-5,-5)).toBe(-10);
 });
 })
